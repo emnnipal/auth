9eea6df69729d595c9b7e639de3c6724cc712e32
emmanuel.nipal@radicalsolutionsinc.com <emmanuel.nipal@radicalsolutionsinc.com>
2017-10-05 16:07:33 +0800

    reusable components

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Header } from './src/components/common';
 
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View>
+        <Header headerText="Authentication" />
         <Text>An App!</Text>
       </View>
     );
