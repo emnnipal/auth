b6c61a4922b48fd73ba8dd5424b1c7d480597868
emmanuel.nipal@radicalsolutionsinc.com <emmanuel.nipal@radicalsolutionsinc.com>
2017-10-05 17:24:59 +0800

    Login"

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './src/components/common';
-
+import LoginForm from './src/components/LoginForm';
 
 export default class App extends Component<{}> {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyDJ4eXfazf_R7iTN9BOICJeZkWC-DdjKrw",
+      authDomain: "auth-8dc06.firebaseapp.com",
+      databaseURL: "https://auth-8dc06.firebaseio.com",
+      projectId: "auth-8dc06",
+      storageBucket: "auth-8dc06.appspot.com",
+      messagingSenderId: "261521980800"
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+import { Button, Card, CardSection} from './common';
+
+class LoginForm extends Component {
+  state = { text: ''};
+  render(){
+    return(
+      <Card>
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 40, width: 200, fontSize: 20 }}
+          />
+        </CardSection>
+        <CardSection>
+
+        </CardSection>
+        <CardSection>
+          <Button>
+            Log in
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
